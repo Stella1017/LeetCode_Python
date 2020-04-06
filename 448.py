91edce70d76cc64f1e4987af5a0605117a638e9e
Stella1017 <list.stella@gmail.com>
2020-04-06 10:37:29 -0700

    Create 448.py

---
+++ 448.py
@@ -0,0 +1,52 @@
+# 448. Find All Numbers Disappeared in an Array (Easy)
+
+# Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+# Find all the elements of [1, n] inclusive that do not appear in this array.
+
+# Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+# Brute Force
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        n = len(nums)
+        s = set(nums)
+        
+        for i in range(1, n+1):
+            if i not in s:
+                res.append(i)
+                
+        return res
+        
+# Advanced
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        # Iterate over each of the elements in the original array
+        for i in range(len(nums)):
+            
+            # Treat the value as the new index
+            new_index = abs(nums[i]) - 1
+            
+            # Check the magnitude of value at this new index
+            # If the magnitude is positive, make it negative 
+            # thus indicating that the number nums[i] has 
+            # appeared or has been visited.
+            if nums[new_index] > 0:
+                nums[new_index] *= -1
+        
+        # Response array that would contain the missing numbers
+        result = []    
+        
+        # Iterate over the numbers from 1 to N and add all those
+        # that have positive magnitude in the array 
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)
+                
+        return result        
