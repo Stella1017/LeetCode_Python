53e63883d25a92aacaeff4ebc131d7adc989576d
Stella1017 <list.stella@gmail.com>
2020-04-07 09:49:00 -0700

    Create 532.py

---
+++ 532.py
@@ -0,0 +1,22 @@
+# 532. K-diff Pairs in an Array
+
+# Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. 
+# Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and 
+# their absolute difference is k.
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        # k is absolute diff, then should be non-negative
+        if k < 0: 
+            return 0
+        
+        d = {}
+        for num in nums:
+            d[num] = d.get(num, 0) + 1
+            
+        res = 0
+        for num, v in d.items():
+            if (k==0 and v>1) or (k!=0 and num+k in d):
+                res += 1
+                
+        return res
