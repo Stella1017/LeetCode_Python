4f5ffd546e0eda7e0fa3e0648d9f27bbcb482692
Stella1017 <list.stella@gmail.com>
2020-05-19 11:26:08 -0700

    Update 268.py

--- 268.py
+++ 268.py
@@ -3,7 +3,7 @@
 # Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
 # find the one that is missing from the array.
 
-# Solution1: HashSet
+# Solution 1: HashSet
 class Solution:
     def missingNumber(self, nums):
         num_set = set(nums)
@@ -12,9 +12,17 @@ class Solution:
             if number not in num_set:
                 return number
 
-# Solution2: Math
+# Solution 2: Math
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        expected_sum = len(nums)*(len(nums)+1)//2
+        expected_sum = len(nums) * (len(nums)+1) // 2
         actual_sum = sum(nums)
         return expected_sum - actual_sum
+
+# Solution 3: Bit Operation
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
