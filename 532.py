9d181a35a2397c988f6a3781137c9369e5ac1012
Stella1017 <list.stella@gmail.com>
2020-05-26 11:26:00 -0700

    Update 532.py

--- 532.py
+++ 532.py
@@ -4,6 +4,7 @@
 # Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and 
 # their absolute difference is k.
 
+# Solution 1: hash set
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
         # k is absolute diff, then should be non-negative
@@ -12,11 +13,28 @@ class Solution:
         
         d = {}
         for num in nums:
-            d[num] = d.get(num, 0) + 1
-            
+            d[num] = d.get(num, 0) + 1        
         res = 0
         for num, v in d.items():
             if (k==0 and v>1) or (k!=0 and num+k in d):
                 res += 1
-                
         return res
+
+# Solution 2: two pointers
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        if k < 0:
+            return 0
+        
+        nums.sort()
+        lo, hi = 0, 1
+        res = set()
+        while lo < len(nums) and hi < len(nums):
+            if nums[lo] + k == nums[hi] and lo != hi:
+                res.add((nums[lo], nums[hi]))
+                hi += 1
+            elif nums[lo] + k < nums[hi]:
+                lo += 1
+            else: 
+                hi += 1
+        return len(res)
