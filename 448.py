e37586c4cd6bf9e702c15e13749d4bc395373015
Stella1017 <list.stella@gmail.com>
2020-05-29 11:37:43 -0700

    Update 448.py

--- 448.py
+++ 448.py
@@ -10,43 +10,22 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         res = []
-        n = len(nums)
         s = set(nums)
-        
-        for i in range(1, n+1):
+        for i in range(1, len(nums) + 1):
             if i not in s:
-                res.append(i)
-                
+                res.append(i)         
         return res
         
-# Advanced
-class Solution(object):
-    def findDisappearedNumbers(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        
-        # Iterate over each of the elements in the original array
-        for i in range(len(nums)):
+# Space O(1)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+            for i in range(len(nums)):
+                new_idx = abs(nums[i]) - 1
+                if nums[new_idx] > 0:
+                    nums[new_idx] *= -1
+            res = []
+            for i in range(1, len(nums) + 1):
+                if nums[i - 1] > 0:
+                    res.append(i)
             
-            # Treat the value as the new index
-            new_index = abs(nums[i]) - 1
-            
-            # Check the magnitude of value at this new index
-            # If the magnitude is positive, make it negative 
-            # thus indicating that the number nums[i] has 
-            # appeared or has been visited.
-            if nums[new_index] > 0:
-                nums[new_index] *= -1
-        
-        # Response array that would contain the missing numbers
-        result = []    
-        
-        # Iterate over the numbers from 1 to N and add all those
-        # that have positive magnitude in the array 
-        for i in range(1, len(nums) + 1):
-            if nums[i - 1] > 0:
-                result.append(i)
-                
-        return result        
+            return res
